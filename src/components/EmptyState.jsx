0a8f420146b61db5f0528b9c6bbde4ef023a6d89
talrukshin <46346186+talrukshin@users.noreply.github.com>
2026-08-03 21:10:17 +0300

    Delete src directory

--- src/components/EmptyState.jsx
+++
@@ -1,9 +0,0 @@
-import "./EmptyState.css";
-
-export default function EmptyState({ message }) {
-  return (
-    <div className="empty-state">
-      <p>{message || "עוד לא נוספו מתכונים לספר. הגיע הזמן להתחיל לבשל!"}</p>
-    </div>
-  );
-}
